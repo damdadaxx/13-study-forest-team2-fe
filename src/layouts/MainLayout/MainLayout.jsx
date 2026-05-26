dceb1d9ec023e81ce87ab98c46265445fd4e006a
apple <ljt2023@gmail.com>
2026-05-26 18:03:54 +0900

    파일 분리 및 수정

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,23 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router';
 
-import Header from '../Header';
+import './MainLayout.css';
 
 export default function MainLayout({ maxWidthType = 'md' }) {
   const maxWidthMap = {
-    sm: 'max-w-[696px]', // 상세페이지
-    md: 'max-w-[1200px]', // 스터디 상세, 홈
-    lg: 'max-w-[1248px]', // 오늘의 집중, 오늘의 습관
+    sm: 'layoutSm', // 상세페이지
+    md: 'layoutMd', // 스터디 상세, 홈
+    lg: 'layoutLg', // 오늘의 집중, 오늘의 습관
   };
 
-  const currentMaxWidth = maxWidthMap[maxWidthType] || maxWidthMap['md'];
+  const currentMaxWidth = maxWidthMap[maxWidthType] || maxWidthMap.md;
 
   return (
-    <div className="min-h-screen bg-[#F6F4EF] flex flex-col">
-      <Header />
+    <div className="layoutContainer">
+      {/* Header  */}
 
-      <main
-        className={`flex-1 w-full mx-auto ${currentMaxWidth} px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-10 lg:px-10 lg:py-12`}
-      >
+      <main className={`layoutMain ${currentMaxWidth}`}>
         <Outlet />
       </main>
     </div>
