15f9692de916811f85e8b2d95463c23b47a4353a
HYOJU <76928618+damdadaxx@users.noreply.github.com>
2026-05-27 13:27:15 +0900

    refactor: Button 컴포넌트 props 수정 (#11)
    - Button 컴포넌트 width props 삭제 후 size props 추가
    - Button 컴포넌트 size props에 따른 스타일 예외처리
    - 제주 돌담체 12pt 변수 추가(--typography-jeju-400-12)
    - ButtonText 컴포넌트 white-space: nowrap 속성 추가

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -5,22 +5,30 @@ import styles from '@/components/common/Button/Button.module.css';
 export default function Button({
   className = '',
   text = '',
+  /**
+   * [size Props]
+   * 1. md
+   * - width: 100%
+   * 컨텐츠의 크기에 맞춰 버튼 크기가 결정
+   * 2. sm
+   * - PC) width: 252px, Tablet) width: 160px, Mobile) width: 106px
+   * - Header 컴포넌트에서 사용
+   */
+  size = 'md',
   color = 'brand', // brand, gray
-  width = '100%',
   type = 'button',
   disabled = false,
   onClick,
   href,
   ...props
 }) {
-  const classNames = `${styles.button} ${styles[color]} ${className}`;
+  const classNames = `${styles.button} ${styles[size]} ${styles[color]} ${className}`;
 
   if (href) {
     return (
       <Link
         to={href}
         className={classNames}
-        style={{ width }}
         type={type}
         onClick={onClick}
         aria-label={`${text} 버튼`}
@@ -35,7 +43,6 @@ export default function Button({
     <>
       <button
         className={classNames}
-        style={{ width }}
         type={type}
         disabled={disabled}
         onClick={onClick}
