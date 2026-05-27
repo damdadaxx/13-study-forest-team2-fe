50fe1185f8a81a136450355d36681b2e7432cc07
jeantae <ljt2023@gmail.com>
2026-05-27 13:37:49 +0900

    MainLayout 공통 스타일 및 Container 컴포넌트 (#9)
    * feat: Outlet 기반의 반응형 공통 MainLayout 컴포넌트 구현
    
    * 파일 분리 및 수정
    
    * layout code fix
    
    * container 파일 추가 및 코드 정리
    
    ---------
    
    Co-authored-by: HYOJU <76928618+damdadaxx@users.noreply.github.com>

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
 
+import styles from '@/layouts/MainLayout/MainLayout.module.css';
+
 import Header from '@/components/common/Header/Header';
 
 export default function MainLayout() {
   return (
-    <main>
+    <main className={styles.styles}>
       <Header />
       <Outlet />
     </main>
