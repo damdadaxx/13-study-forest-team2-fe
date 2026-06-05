d1a0cf1fafb9ff3d969d0a940496b1df14be0274
choihoomba <talote33@naver.com>
2026-06-05 15:36:35 +0900

    fix:스터디 만들기 버튼 예외 처리 (#40)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,11 +1,14 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 
 import Button from '@/components/common/Button/Button';
 import styles from '@/components/common/Header/Header.module.css';
 
 import forestLogo from '@/assets/images/icons/forest-logo.png';
 
-export default function Header({ hasButton = true }) {
+export default function Header() {
+  const { pathname } = useLocation();
+  const hideButton = pathname === '/studies/new';
+
   return (
     <header className={styles.forestHeader}>
       <div className={styles.container}>
@@ -17,10 +20,10 @@ export default function Header({ hasButton = true }) {
           />
         </Link>
 
-        {hasButton && (
+        {!hideButton && (
           <Button text="스터디 만들기" size="sm" href="/studies/new" />
         )}
       </div>
     </header>
-  );  
+  );
 }
